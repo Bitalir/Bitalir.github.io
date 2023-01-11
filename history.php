756ada25991cd8d06d3fb5d628c2c89fff87a58d
blackhole <121114560+Bitalir@users.noreply.github.com>
2023-01-11 21:10:00 +0300

    Add files via upload

---
+++ history.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Чёрная дыра</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About" />
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering." />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>История</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Прочти, если хочешь узнать историю чёрных дыр
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Эпоха чёрных дыр</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									Впервые предположение о существовании подобных областей во Вселенной выдвинул английский священник Д. Мичел в 1783 году. В свободное время он увлекался астрономией. Согласно его расчётам, если сжать астрофизический объект, то возникнет мощнейшая гравитационная сила. При этом его пределы не сможет покинуть даже свет. <br>
+									<br>
+									 Кроме того, подобную гипотезу выдвинул французский учёный Пьер Лаплас. Он считал, что в случае высокой гравитации тела скорость убегания больше световой скорости. То есть частицы света двигающиеся в точку не смогут двигаться бесконечно, и будут удерживаться гравитационной силой.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/history/1.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/history/2.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 Спустя много лет в 1915 году Альберт Эйнштейн написал основные уравнения общей теории относительности. Они также предсказывали наличие особых областей в космическом пространстве.<br>
+
+									<br>В 1916 году на основе общей теории относительности Карл Шварцшильд расчитал числовое значение пространства и времени внутри и за пределами тела, влияющее на движение частиц и античастиц.<br>
+									<br>Между тем, официально термин «чёрная дыра» был введён в 1968 году благодаря труду американского физика-теоретика Джона Уиллера. Он описал свойства этих областей, использовав новое для них понятие.<br>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Обнаружение чёрной дыры</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>Хотя они обладают огромной массой и гравитацией, обнаружение чёрных дыр с помощью телескопа практически невозможно. Дело в том, что они не излучают свет, а значит сливаются с тёмным небом.<br>
+								<br>Однако учёные смогли увидеть её, когда она поглощала вещество звезды. При этом появилось особенное и яркое излучение.
+								Можно сказать, что настоящее открытие области пространства-времени произошло в 1971 году. Тогда чёрная дыра Лебедь Х-1 была замечена в телескоп.<br>
+								<br>По правде говоря, история открытия и изучения чёрных дыр длилась очень долго и продолжается по сей день. Разумеется, мы прошли непростой путь от теории до их фактического обнаружения. Вероятно, в будущем нас ждут новые открытия. Мы, наконец, узнаем всё про эти таинственные области космоса.<br>
+			                  	</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/history/3.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
